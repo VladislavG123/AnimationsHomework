4c4a410f4ffafad1244957d375a5b09a61313931
Владислав Горохов <gbg.0101@mail.ru>
2019-05-23 16:47:29 +0600

    Initial

---
+++ AnimationsHomeWork/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimationsHomeWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var animation = new ThicknessAnimation
+            {
+                By = new Thickness(canvas.Width, 0, 0, 0),
+                Duration = new Duration(TimeSpan.FromSeconds(1)),
+                RepeatBehavior = RepeatBehavior.Forever,
+                AutoReverse = true
+            };
+            rectangle.BeginAnimation(MarginProperty, animation);
+        }
+    }
+}
